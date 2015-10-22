944811e540e67a1ef1262163e1e4bae13902dcb6
Gregory Wehmeier <wehmenstein@gmail.com>
2015-10-22 10:55:32 -0400

    add work for 4.5.5

---
+++ week-4/simple-substrings/my_solution.rb
@@ -0,0 +1,13 @@
+# Simple Substrings
+
+# I worked on this challenge by myself.
+
+# Your Solution BelowIn the my_solution.rb file:
+
+def welcome (address)
+  if address.include?('CA')
+    return 'Welcome to California'
+  else
+    return 'You should move to California'
+  end
+end
