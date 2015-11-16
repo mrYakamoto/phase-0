f27d9c2b141b29a522bf815e8625cef920679e4b
Gregory Wehmeier <wehmenstein@gmail.com>
2015-11-15 19:51:42 -0600

    update javascript game

--- week-7/game.js
+++ week-7/game.js
@@ -1,20 +1,4 @@
-<<<<<<< HEAD
-// MY DUEL GAME
-// CHARACTERS:
 
-// KNIGHT -
-  // health
-  // attacks - slash, jab, shield
-  // specials - wizard's potion
-
-// COWBOY
-  // health
-  // attacks - pistol, knife, dodge
-  // specials - bullwhip
-
-
-console.log("Welcome to the duel");
-=======
 // LUMBERJACK
 
 // Lumberjack1 (user)
@@ -46,10 +30,10 @@ var forest = {
     redwood: 80
   },
   hazards: {
-    poisonIvy: 5,
-    squirrelAttack: 6,
-    skunked: 10,
-    bear: 50
+    poisonIvy: 15,
+    squirrelAttack: 25,
+    skunked: 30,
+    bear: 80
   }
 };
 
@@ -67,23 +51,24 @@ var chainsaw = {
 };
 
 // Driver Code
-// startGame();
-// surveyForest();
-// cutTree('dogwood1');
-// surveyForest();
-// cutTree('dogwood2');
-// surveyForest();
-// cutTree('pine1');
-// surveyForest();
-// buyTool(axe);
-// cutTree('pine2');
-// cutTree('pine3');
-// cutTree('oak1');
-// cutTree('oak2');
-// buyTool(chainsaw);
-// surveyForest();
-// cutTree('redwood');
-// surveyForest();
+startGame();
+surveyForest();
+cutTree('dogwood1');
+surveyForest();
+buyTool(axe);
+cutTree('dogwood2');
+surveyForest();
+cutTree('pine1');
+surveyForest();
+buyTool(axe);
+cutTree('pine2');
+cutTree('pine3');
+cutTree('oak1');
+cutTree('oak2');
+buyTool(chainsaw);
+surveyForest();
+cutTree('redwood');
+surveyForest();
 
 
 function startGame(){
@@ -109,7 +94,7 @@ function cutTree(tree) {
   console.log("It took you " + (forest.trees[tree] / lumberjack.power ) + " minutes to fall " + tree + ". It yielded you " + forest.trees[tree] + " units of lumber.\n");
   delete forest.trees[tree];
   chanceOfHazard();
-  checkHealth;
+  checkHealth();
 }
 
 
@@ -121,39 +106,36 @@ function buyTool(newTool){
     console.log("Congratulations on your new " + newTool.id + ", use it well.\n");
   }
   else {
-    console.log("Nah, you don't have enough for that. I'll trade you for " + newTool.cost + ".");
+    console.log("Nah, you don't have enough for that. You need " + (newTool.cost - lumberjack.lumber) +  " more units of lumber.\n");
   }
 }
 
 function chanceOfHazard() {
  var randNumber = Math.floor((Math.random() * 100) + 1);
  if (randNumber < 2){
-   lumberjack.health -= 50;
-   console.log("Ohhhh nooooo! You got attacked by a bear! You lost 50 health points");
+   lumberjack.health -= forest.hazards.bear;
+   console.log("Ohhhh nooooo! You got attacked by a bear! You lost " + forest.hazards.bear + " health points");
    console.log("Current Health: " + lumberjack.health + "\n");
  }
  else if (randNumber < 10){
-   lumberjack.health -= 10;
-   console.log("What's that smell. Aww mannn, a skunk sprayed you. You lost 10 health points");
+   lumberjack.health -= forest.hazards.skunked;
+   console.log("What's that smell. Aww mannn, a skunk sprayed you. You lost " + forest.hazards.skunked + " health points");
    console.log("Current Health: " + lumberjack.health + "\n");
  }
  else if (randNumber < 30){
-   lumberjack.health -= 6;
-   console.log("You got attacked by a squirrel. You lost 6 health points.");
+   lumberjack.health -= forest.hazards.squirrelAttack;
+   console.log("You got attacked by a squirrel. You lost " + forest.hazards.squirrelAttack + " health points.");
    console.log("Current Health: " + lumberjack.health + "\n");
  }
  else if (randNumber < 50){
-   lumberjack.health -= 3;
-   console.log("Some poison ivy got on you. You lost 3 health points.");
+   lumberjack.health -= forest.hazards.poisonIvy;
+   console.log("Some poison ivy got on you. You lost " + forest.hazards.poisonIvy + " health points.");
    console.log("Current Health: " + lumberjack.health + "\n");
  }
 }
 
 function checkHealth(){
-  if (lumberjack.health < 0) {
-    console.log("Aw snap, you tragically died.")
+  if (lumberjack.health <= 0) {
+    throw new Error("You have tragically died in the name of lumberjacking");
   }
 }
-
-
->>>>>>> 7.6
