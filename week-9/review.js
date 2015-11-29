0d8ba841c29bf3af9bc73fe5d2c49080a47735ce
Gregory Wehmeier <wehmenstein@gmail.com>
2015-11-29 12:35:07 -0800

    add 9.2 exercise

--- week-9/review.js
+++ week-9/review.js
@@ -1,3 +1,62 @@
+
+// GROCERY LIST IN JAVASCRIPT
+class GroceryList {
+  constructor(store) {
+    this.store = store;
+  }
+  addItem(item, quantity){
+    if (this.hasOwnProperty(item)) {
+      return "That's already on the list."
+    }
+    else {
+      this[item] = quantity;
+    }
+  }
+  removeItem(item){
+    if (this[item] === undefined){
+      return "That item's not on the list yet.";
+      }
+    delete this[item];
+    return "Removed " + item + " from the list."
+  }
+  changeQuantity(item, quantity){
+    if (this[item] === undefined){
+      return "That item's not on the list yet."
+    }
+    this[item] = quantity;
+    return item + ": " + quantity.toString();
+  }
+  printList(){
+    console.log("\n"+this.store + " Grocery List");
+    for (var key in this){
+      if (key == "store"){
+      }
+      else{
+      console.log(key + ": " + this[key]);
+      }
+    }
+  }
+
+}
+
+// DRIVER CODE
+var tday = new GroceryList("Whole Foods");
+console.log(tday);
+tday.addItem("milk", 1);
+console.log(tday.addItem("milk", 2));
+tday.addItem("banana", 3);
+tday.addItem("eggs", 12);
+tday.addItem("spam", 4);
+console.log(tday);
+console.log(tday.removeItem("milk"));
+console.log(tday["milk"]);
+console.log(tday.changeQuantity("banana",2));
+console.log(tday.banana);
+tday.printList();
+
+
+
+
 // -Basic functionality
 // Create a new list
 // Add an item with a quantity to the list
@@ -51,61 +110,3 @@ class CreditCard {
 
 var card = new CreditCard(4563960122001999);
 console.log(card.checkCard());
-
-
-// GROCERY LIST IN JAVASCRIPT
-class GroceryList {
-  constructor(store) {
-    this.store = store;
-  }
-  addItem(item, quantity){
-    if (this.item) {
-      return "That's already on the list."
-    }
-    else {
-      this[item] = quantity;
-    }
-  }
-  removeItem(item){
-    if (this[item] === undefined){
-      return "That item's not on the list yet.";
-      }
-    delete this[item];
-    return "Removed " + item + " from the list."
-  }
-  changeQuantity(item, quantity){
-    if (this[item] === undefined){
-      return "That item's not on the list yet."
-    }
-    this[item] = quantity;
-    return item + ": " + quantity.toString();
-  }
-  printList(){
-    console.log("\n"+this.store + " Grocery List");
-    for (var key in this){
-      if (key == "store"){
-      }
-      else{
-      console.log(key + ": " + this[key]);
-      }
-    }
-  }
-
-}
-
-// DRIVER CODE
-var tday = new GroceryList("Whole Foods");
-console.log(tday);
-tday.addItem("milk", 1);
-tday.addItem("banana", 3);
-console.log(tday);
-console.log(tday.removeItem("milk"));
-console.log(tday["milk"]);
-console.log(tday.changeQuantity("banana",2));
-console.log(tday.banana);
-tday.printList();
-
-
-
-
-
